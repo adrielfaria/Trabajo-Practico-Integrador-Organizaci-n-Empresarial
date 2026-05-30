208680076bdc189b8bbccc315a5cb680bc88b9fd
Adriel <adrielfa82@gmail.com>
2026-05-30 19:37:56 -0300

    eliminar funcion bienvenida()

--- bot_gestion_vacaciones.py
+++ bot_gestion_vacaciones.py
@@ -6,14 +6,12 @@ with open("empleados.json", "r", encoding="utf-8") as f:
 
 print(indice)
 
-
-def bienvenida():
-    print('👋 Bienvenido al Bot de Vacaciones')
-    print('Por favor ingrese su legajo')
-    return input()
+print('👋 Bienvenido al Bot de Vacaciones')
+print('Por favor ingrese su legajo')
+legajo = input()
 
 def consultar_saldo(legajo):
     print(f'Su saldo de dias disponible es de {indice[legajo]['dias_disponibles']}')
 
 
-consultar_saldo(bienvenida())
+consultar_saldo(legajo)
