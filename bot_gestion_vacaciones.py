566f93525d7987ae5b13bf59f73f1a978d60a1a6
Adriel <adrielfa82@gmail.com>
2026-05-30 20:01:43 -0300

    Agregar modulo datetime

--- bot_gestion_vacaciones.py
+++ bot_gestion_vacaciones.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 with open("empleados.json", "r", encoding="utf-8") as f:
     indice = json.load(f)
@@ -14,12 +15,16 @@ def consultar_saldo(legajo):
 
 
 def solicitar_vaciones(legajo):
-    inicio = input('Ingresa la fecha de inicio(DD/MM/AAAA) ')
-    fin = input('Ingresa la fecha de fin(DD/MM/AAAA) ')
+    fecha_inicio = input('Ingresa la fecha de inicio(DD/MM/AAAA) ')
+    fecha_fin = input('Ingresa la fecha de fin(DD/MM/AAAA) ')
+    inicio = datetime.strptime(fecha_inicio, '%d/%m/%Y')
+    fin = datetime.strptime(fecha_fin, '%d/%m/%Y')
+    dias_solicitados = (fin - inicio).days + 1
     print('Resumen')
     print(indice[legajo]['nombre'])
     print(f'Desde: {inicio}')
     print(f'Hasta: {fin}')
+    print(f'Dias: {dias_solicitados}')
     confirmacion = input('Confirmas? Escribe SI o NO: ')
     
 
