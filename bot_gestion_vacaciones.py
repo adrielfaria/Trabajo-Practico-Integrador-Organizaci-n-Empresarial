f0a4c1791ef2ee2b9a200b31f8e17e0b366721e3
Adriel <adrielfa82@gmail.com>
2026-05-30 19:41:58 -0300

    crear funcion solicitar_vacaciones()

--- bot_gestion_vacaciones.py
+++ bot_gestion_vacaciones.py
@@ -7,11 +7,15 @@ with open("empleados.json", "r", encoding="utf-8") as f:
 print(indice)
 
 print('👋 Bienvenido al Bot de Vacaciones')
-print('Por favor ingrese su legajo')
-legajo = input()
+legajo = input('Por favor ingrese su legajo: ')
 
 def consultar_saldo(legajo):
     print(f'Su saldo de dias disponible es de {indice[legajo]['dias_disponibles']}')
 
+
+def solicitar_vaciones(legajo):
+    inicio = input('Ingresa la fecha de inicio(DD/MM/AAAA) ')
+    fin = input('Ingresa la fecha de fin(DD/MM/AAAA) ')
+    
 
 consultar_saldo(legajo)
