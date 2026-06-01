65b1c4b1da1b35204786a8a57be2cdbbf1e54385
Adriel <adrielfa82@gmail.com>
2026-06-01 01:31:20 -0300

    modificar funcion solicitar_vacaciones

--- bot_gestion_vacaciones.py
+++ bot_gestion_vacaciones.py
@@ -4,10 +4,6 @@ from datetime import datetime
 with open("empleados.json", "r", encoding="utf-8") as f:
     indice = json.load(f)
 
-
-print(indice)
-
-
 def consultar_saldo(legajo):
     print(f'Su saldo de dias disponible es de {indice[legajo]['dias_disponibles']}')
 
@@ -19,12 +15,19 @@ def solicitar_vaciones(legajo):
         inicio = datetime.strptime(fecha_inicio, '%d/%m/%Y')
         fin = datetime.strptime(fecha_fin, '%d/%m/%Y')
         dias_solicitados = (fin - inicio).days + 1
+        dias_restantes = indice[legajo]['dias_disponibles'] - dias_solicitados
         print('Resumen')
         print(indice[legajo]['nombre'])
         print(f'Desde: {inicio}')
         print(f'Hasta: {fin}')
         print(f'Dias: {dias_solicitados}')
+        print(f'Nuevo saldo de dias restantes: {dias_restantes}')
         confirmacion = input('Confirmas? Escribe SI o NO: ')
+        if confirmacion == 'SI':
+            indice[legajo]['dias_disponibles'] = dias_restantes
+
+    except ValueError:
+        print('Ingrese una fecha valida.')
     except Exception as e:
         print(f'Error: {e}')
 
@@ -35,8 +38,8 @@ def main():
     print('👋 Bienvenido al Bot de Vacaciones')
     legajo = input('Por favor ingrese su legajo: ')
     opciones = {
-        '1': consultar_saldo,
-        '2': solicitar_vaciones,
+        '1': lambda: consultar_saldo(legajo),
+        '2': lambda: solicitar_vaciones(legajo),
         '3': salir
     }
 
@@ -50,7 +53,7 @@ def main():
         funcion = opciones.get(opcion)
 
         if funcion:
-            resultado = funcion(legajo)
+            resultado = funcion()
             if resultado is False:
                 break
     
